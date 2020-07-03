9bd00c31c6f3b6d8230eea64eab904e6d8e272f7
adarshreddyash <adarshreddy9849@gmail>
2020-07-03 16:15:38 +0530

    hai

--- api/settings.py
+++ api/settings.py
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'rest_auth',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
     'music'
 ]
 
